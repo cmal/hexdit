ffe9fee4129dd0101cb65c05f669446c4e51a22d
ahonn <ahonn95@outlook.com>
2017-09-11 11:16:45 +0800

    feat: add create page button

--- src/renderer/component/create-blog-form.cljs
+++ src/renderer/component/create-blog-form.cljs
@@ -1,6 +1,10 @@
 (ns renderer.component.create-blog-form
   (:require [reagent.core :as reagent]
-            [antizer.reagent :as ant]))
+            [antizer.reagent :as ant]
+            [secretary.core :as secretary]))
+
+(defn cancel-create []
+  (secretary/dispatch! "/"))
 
 (defn create-form []
   (fn [props]
@@ -8,15 +12,23 @@
       [ant/form {:class "create-blog-form"}
         [ant/form-item {:label "博客名称"}
           (ant/decorate-field form "name" {:rules [{:required true} {:type "text"}]}
-            [ant/input {:prefix (reagent/as-element
+            [ant/input {:size "large"
+                        :prefix (reagent/as-element
                                   [ant/icon {:type "book"}])}])]
         [ant/form-item {:label "博客路径"}
           (ant/decorate-field form "path" {:rules [{:required true} {:type "text"}]}
-            [ant/input {:prefix (reagent/as-element
+            [ant/input {:size "large"
+                        :prefix (reagent/as-element
                                   [ant/icon {:type "folder"}])}])]
         [ant/form-item {:label "博客描述"}
           (ant/decorate-field form "description" {:rules [:type "text"]}
-            [ant/input-text-area {:rows 4}])]])))
+            [ant/input-text-area {:rows 4}])]
+        [ant/form-item
+          [:div {:class "create-button-group"}
+            [ant/button {:on-click #(cancel-create)}
+              "返回"]
+            [ant/button {:type "primary"}
+              "确认"]]]])))
 
 (defn create-blog-form-component []
    (ant/create-form (create-form)))
