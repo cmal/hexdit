9f91ff46b524f1b13fdbe0a1a6deaebc8e036b0b
ahonn <ahonn95@outlook.com>
2017-09-18 17:23:25 +0800

    chore: adjust format blog-list.cljs

--- src/renderer/component/blog-list.cljs
+++ src/renderer/component/blog-list.cljs
@@ -37,10 +37,10 @@
 
 (defn blog-control [idx title]
   [:div {:class "blog-control"}
-    [ant/icon {:type "edit"
+   [ant/icon {:type "edit"
               :class "edit"
               :on-click #(edit-blog % idx)}]
-    [ant/icon {:type "delete"
+   [ant/icon {:type "delete"
               :class "delete"
               :on-click #(show-modal % idx title)}]])
 
@@ -48,23 +48,23 @@
   (let [title (get blog-info "title")
         description (get blog-info "description")]
     [ant/card {:class "blog-item"
-             :bodyStyle {:padding "15px"}
-             :noHovering true
-             :bordered false
-             :extra (reagent/as-element
-                      [blog-control idx title])
-             :on-click #(open-blog blog-info)}
-      [:h2 {:class "blog-title"} title]
-      [:p {:class "blog-description"} description]]))
+               :bodyStyle {:padding "15px"}
+               :noHovering true
+               :bordered false
+               :extra (reagent/as-element
+                        [blog-control idx title])
+               :on-click #(open-blog blog-info)}
+     [:h2 {:class "blog-title"} title]
+     [:p {:class "blog-description"} description]]))
 
 (defn blog-list-component []
   [:div {:class "blog-list"}
-    (map-indexed
-      (fn [idx blog-info]
-        ^{:key (str idx (get blog-info "path"))}
-        [blog-list-item idx blog-info])
-      @(rf/subscribe [:blog-list]))
-    [ant/modal {:title "删除博客"
+   (map-indexed
+     (fn [idx blog-info]
+       ^{:key (str idx (get blog-info "path"))}
+       [blog-list-item idx blog-info])
+     @(rf/subscribe [:blog-list]))
+   [ant/modal {:title "删除博客"
                :visible (:visible @modal-state)
                :onOk #(remove-blog % (:idx @modal-state) (:blog-title @modal-state))
                :onCancel #(hide-modal %)}
