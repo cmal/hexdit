f9674810053f02fb9fe48ac894e22dd82bf357d9
ahonn <ahonn95@outlook.com>
2017-09-26 11:16:33 +0800

    feat: add open blog folder

--- src/renderer/component/shortcuts.cljs
+++ src/renderer/component/shortcuts.cljs
@@ -4,16 +4,23 @@
             [antizer.reagent :as ant]
             [secretary.core :as secretary]))
 
+(def electron (js/require "electron"))
+(def shell (.-shell electron))
+
 (defn switch-blog []
   (rf/dispatch-sync [:switch-blog nil])
   (secretary/dispatch! "/"))
+
+(defn open-blog-folder []
+  (let [folder-path (get @(rf/subscribe [:current-blog]) "path")]
+    (.openExternal shell (str "file://" folder-path))))
 
 (def shortcuts-option [{:icon "retweet"
                         :text "切换博客"
                         :action #(switch-blog)}
                        {:icon "folder"
                         :text "打开文件夹"
-                        :action #()}
+                        :action #(open-blog-folder)}
                        {:icon "link"
                         :text "本地预览"
                         :action #()}
